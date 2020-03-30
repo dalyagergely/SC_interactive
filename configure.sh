5725feb38f6bf2d959ff1aa8167c579ca0eb37ee
dalyagergely <dalyagergely@gmail.com>
2020-03-30 13:38:17 -0500

    Fixed to work with newest pygwinc version

--- configure.sh
+++ configure.sh
@@ -12,16 +12,22 @@ echo "Please enter the name of your new detector"
 
 read name
 
-cp -r $pygwinc/gwinc/ifo/aLIGO $pygwinc/gwinc/ifo/$name
+
+if grep -q $name $pygwinc/gwinc/ifo/__init__.py
+then
+    echo "Detector already exists."
+else
+    cp -r $pygwinc/gwinc/ifo/aLIGO $pygwinc/gwinc/ifo/$name
+    sed -i "s@^]@\\t\'$name\',\\n]@" $pygwinc/gwinc/ifo/__init__.py
+fi
 
 
 sed -i "s@^sys.path.insert(.*)@sys.path.insert(1, '$pygwinc')@" SC_interactive.py
 sed -i "s@^ifodir = .*@ifodir = '$pygwinc/gwinc/ifo/$name'@" SC_interactive.py
-sed -i "s@Budget, ifo,.*@Budget, ifo, freq_, plot_style = gwinc.load_ifo('$name')@" SC_interactive.py
+sed -i "s@Budget = .*@Budget = gwinc.load_budget('$name')@" SC_interactive.py
 
 sed -i "s@^class.*@class $name(nb.Budget):@" $pygwinc/gwinc/ifo/$name/__init__.py
 sed -i "s@Advanced LIGO@$name@" $pygwinc/gwinc/ifo/$name/__init__.py
 
-sed -i "s@^]@\\t\'$name\',\\n]@" $pygwinc/gwinc/ifo/__init__.py"
 
 echo "Done!"
